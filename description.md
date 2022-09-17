32a2d542eb19372cf41c968035b1072c2c76f219
Davidzf21 <zandundo2a@gmail.com>
2022-09-17 18:10:45 +0200

    File Description modified and Integration Test description

--- description.md
+++ description.md
@@ -1,3 +1,3 @@
 # Description of the contents of this project
 
-Complete me!
+IntegrationTest.kt ---> File that containing the integration tests. The purpose of integration testing is to confirm that different components of the application interact with each other. Integration testing is considered complete, when actual results and expected results are same. In this case, we want to check that the modified client connects correctly to the server
--- src/test/kotlin/IntegrationTest.kt
+++ src/test/kotlin/IntegrationTest.kt
@@ -10,15 +10,28 @@ import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 
-
+/*  
+    Integration test for the class LAB1-GIT-RACE/src/main/kotlin/controller/IntegrationTest.kt
+    - @SpringBootTest annotation provides a convenient way to start up an application context 
+      to be used in a test. 
+    - @LocalServerPort is a way to define a value for the tomcat port of our service. 
+*/
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class IntegrationTest {
     @LocalServerPort
     private var port: Int = 0
 
+    /* 
+        TestRestTemplate is an alternative that simplifies integration testing and facilitates 
+        authentication during tests. It helps in customization of Apache HTTP client. 
+    */
     @Autowired
     private lateinit var restTemplate: TestRestTemplate
 
+    /*
+        Test that creates a modified Apache HTTP client that connects to the server address and checks that the code of
+        status and the body returned by the server is correct.
+     */
     @Test
     fun testHome() {
         with(restTemplate.getForEntity("http://localhost:$port", String::class.java)) {
