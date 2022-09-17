081517dd853f700d7f02ef0116b325a8ef860271
HectorRute98 <hectorgarciapiqueras98@gmail.com>
2022-09-17 13:31:39 +0200

    First documentation v.0-Héctor

--- src/main/kotlin/controller/HelloController.kt
+++ src/main/kotlin/controller/HelloController.kt
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 
+/* This class HelloController.kt creates a variable of type map and implements a function 
+    called "welcome" which recieves a MutableMap<String,Any> where the function writes the message created before
+    and returns a String with "welcome". */
 @Controller
 class HelloController {
     @Value("\${app.message}")
--- src/test/kotlin/controller/HelloControllerUnitTests.kt
+++ src/test/kotlin/controller/HelloControllerUnitTests.kt
@@ -4,12 +4,17 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
+/*  Unit test for the class LAB1-GIT-RACE/src/main/kotlin/controller/HelloController.kt
+    which is designed using JUnit5 with a @BeforeEach label(methods that are executed before the
+    execution of each of the test methods) for create de controller. */
 class HelloControllerUnitTests {
     private lateinit var controller: HelloController
     @BeforeEach
     fun setup() {
         controller = HelloController()
     }
+    /* Unit method to test the functionality of the controller where we create values for him
+    and we check if the results obtained are the results expected */
     @Test
     fun testMessage() {
         val map = mutableMapOf<String,Any>()
