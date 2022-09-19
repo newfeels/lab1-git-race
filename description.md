6f2065c3ed2d03a5a425c8a9702bb1ecfaae4263
Davidzf21 <zandundo2a@gmail.com>
2022-09-19 11:40:49 +0200

    @Davidzf21

--- description.md
+++ description.md
@@ -1,3 +1,4 @@
 # Description of the contents of this project
 
-IntegrationTest.kt ---> File that containing the integration tests. The purpose of integration testing is to confirm that different components of the application interact with each other. Integration testing is considered complete, when actual results and expected results are same. In this case, we want to check that the modified client connects correctly to the server
+#### IntegrationTest.kt description
+File that containing the integration tests. The purpose of integration testing is to confirm that different components of the application interact with each other. Integration testing is considered complete, when actual results and expected results are same. In this case, we want to check that the modified client connects correctly to the server
--- src/test/kotlin/IntegrationTest.kt
+++ src/test/kotlin/IntegrationTest.kt
@@ -10,27 +10,32 @@ import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 
-/*  
-    Integration test for the class LAB1-GIT-RACE/src/main/kotlin/controller/IntegrationTest.kt
-    - @SpringBootTest annotation provides a convenient way to start up an application context 
-      to be used in a test. 
-    - @LocalServerPort is a way to define a value for the tomcat port of our service. 
-*/
+/**  
+ *  Integration test for the class LAB1-GIT-RACE/src/main/kotlin/controller/IntegrationTest.kt
+ *
+ *  Collection of integration tests to check the operation of the server.
+ *
+ *  @param webEnvironment creates a web application context with triggers usually listening on a random port.
+ *  @param port local server port
+ *  @property SpringBootTest provides a convenient way to start up an application context to be used in a test.
+ *  @property LocalServerPort this is a way to define a value for the tomcat port of our service.
+ *  @property TestRestTemplate this is an alternative that simplifies integration testing and facilitates 
+ *      authentication during tests. It helps in customization of Apache HTTP client.
+ */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class IntegrationTest {
     @LocalServerPort
     private var port: Int = 0
 
-    /* 
-        TestRestTemplate is an alternative that simplifies integration testing and facilitates 
-        authentication during tests. It helps in customization of Apache HTTP client. 
-    */
     @Autowired
     private lateinit var restTemplate: TestRestTemplate
 
-    /*
-        Test that creates a modified Apache HTTP client that connects to the server address and checks that the code of
-        status and the body returned by the server is correct.
+    /**  
+     *  Tests that creates a modified Apache HTTP client that connects to the server address and checks that the code of
+     *      status and the body returned by the server is correct.
+     *
+     *  @property assertThat is one of the JUnit methods from the Assert object that can be used to check if a specific 
+     *      value match to an expected one. It primarily accepts 2 parameters. First one if the actual value and the second is a matcher object.
      */
     @Test
     fun testHome() {
