bf916c08a1e5c7930ad6f13816d040175c1fc637
motinsa <pedroaibarmarco@gmail.com>
2022-09-26 11:50:14 +0200

    fixed the issue

--- src/test/kotlin/controller/HelloControllerUnitTests.kt
+++ src/test/kotlin/controller/HelloControllerUnitTests.kt
@@ -7,12 +7,13 @@ import org.junit.jupiter.api.Test
 class HelloControllerUnitTests {
     /**
      * Variable [controller]
-     takes the value of es.unizar.webeng.hello.controller
+     * takes the value of es.unizar.webeng.hello.controller
      */
     private lateinit var controller: HelloController
     /**
      * The [BeforeEach] annottion is used to signal that the annotated method should
-    be executed before each [Test] */
+     * be executed before each [Test] 
+     */
     @BeforeEach
     /**
      * Declare a function which setup the controller */
@@ -20,23 +21,25 @@ class HelloControllerUnitTests {
         controller = HelloController()
     }
     /** Integration test that calls the function [testMessage]  which create an
-    empty new *mutableMap* of [String] and [Any] (Indicates whether some other
-    object is "equal to" this one). Then calls the function welcome of the 
-    [controller] and assert the result.*/
+     * empty new *mutableMap* of [String] and [Any] (Indicates whether some other
+     * object is "equal to" this one). Then calls the function welcome of the 
+     * [controller] and assert the result.
+    */
     @Test
     fun testMessage() {
         val map = mutableMapOf<String,Any>()
         val view = controller.welcome(map)
-        /** Compare [view] with the string "welcome"
-         * */
+        /** 
+         * Compare [view] with the string "welcome"
+         */
         assertThat(view).isEqualTo("welcome")
         /**
          * Check if the mutable [map] contais the key  "message"
-         * */
+         */
         assertThat(map.containsKey("message")).isTrue
         /**
          * Check if the value of the key [message] is equal to "Hello World"
-         * */
+         */
         assertThat(map["message"]).isEqualTo("Hello World")
     }
 
